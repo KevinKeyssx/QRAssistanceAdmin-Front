90d18d25dc72692f3bd2e85807e8ae14b2033cd2
kevinkeyssx <keyss1700@gmail.com>
2026-04-12 01:19:09 -0400

    Member delete server added

---
+++ src/routes/api/members/delete/+server.ts
@@ -0,0 +1,23 @@
+import { json }          from '@sveltejs/kit';
+import type { 
+    RequestHandler 
+}                       from './$types';
+import connectRequest   from '$lib/services/fetch.service';
+import { METHOD }       from '$lib/services/http-codes';
+
+
+export const DELETE: RequestHandler = async ({ url }) => {
+    const id = url.searchParams.get( 'id' );
+
+    if ( !id ) {
+        return json({ error: 'ID is required' }, { status: 400 });
+    }
+
+    const data = await connectRequest({
+        endpoint    : `v1/members/${ id }`,
+        method      : METHOD.DELETE,
+        isInternal  : false
+    });
+
+    return json( data );
+};
