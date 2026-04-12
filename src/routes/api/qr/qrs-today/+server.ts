df835522afc6ca827fc62813dfaed418d55a9983
kevinkeyssx <keyss1700@gmail.com>
2026-04-12 01:21:36 -0400

    QR get todarys server added

---
+++ src/routes/api/qr/qrs-today/+server.ts
@@ -0,0 +1,19 @@
+import { json } from '@sveltejs/kit';
+
+import type { 
+    RequestHandler 
+}                       from './$types';
+import connectRequest   from '$lib/services/fetch.service';
+import { METHOD }       from '$lib/services/http-codes';
+
+
+export const POST: RequestHandler = async () => {
+    const data = await connectRequest({
+        endpoint    : 'v1/qrs/sunday',
+        method      : METHOD.POST,
+        isInternal  : false,
+        body        : {}
+    });
+
+    return json( data );
+};
