819cdb12c0ffe0056eb369e868dd5e1017f00920
kevinkeyssx <keyss1700@gmail.com>
2026-04-12 01:24:46 -0400

    Survey page added

---
+++ src/routes/dashboard/survey/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>Encuestas | Dashboard | QR Assistance</title>
+</svelte:head>
+
+
+<main class="flex flex-col gap-6 w-full max-w-6xl mx-auto align-top">
+	<h1 class="text-3xl font-bold text-gray-900 dark:text-white">Encuestas</h1>
+</main>
