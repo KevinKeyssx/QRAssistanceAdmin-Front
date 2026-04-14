2ced69ae24ace238bc61125fdca4fc0f02fe0ea4
kevinkeyssx <keyss1700@gmail.com>
2026-04-14 00:14:14 -0400

    ENDPOINT const added

---
+++ src/lib/utils/consts/endpoints.ts
@@ -0,0 +1,9 @@
+export const ENDPOINT = {
+    ANALYTICS: {
+        GROWTH          : 'analytics/growth',
+        PUNCTUALITY     : 'analytics/punctuality',
+        RETENTION       : 'analytics/retention',
+        LOYALTY         : 'analytics/loyalty',
+        MAIN            : 'analytics/main-chart'
+    }
+}
