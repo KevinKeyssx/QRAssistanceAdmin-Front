8ceb5cf6d1a74da34897f508bac0903323e67e27
kevinkeyssx <keyss1700@gmail.com>
2026-04-14 00:13:52 -0400

    Links consts added

---
+++ src/lib/components/dashboard/links.ts
@@ -0,0 +1,8 @@
+export const links = [
+    { href: '/dashboard',              label: 'Inicio' },
+    { href: '/dashboard/qr-generator',  label: 'Generador QR' },
+	{ href: '/dashboard/members',       label: 'Miembros' },
+	{ href: '/dashboard/attendance',    label: 'Asistencias' },
+	{ href: '/dashboard/analytics',     label: 'Analíticas' },
+	{ href: '/dashboard/survey',        label: 'Encuestas' }
+];
