c4e376be893ccef6acd12fd697455705becccf09
kevinkeyssx <keyss1700@gmail.com>
2026-04-15 21:54:56 -0400

    Delete assistance server added

---
+++ src/routes/api/assistances/delete/+server.ts
@@ -0,0 +1,24 @@
+import { json }          from '@sveltejs/kit';
+
+import type { 
+    RequestHandler 
+}                       from './$types';
+import connectRequest   from '$lib/services/fetch.service';
+import { METHOD }       from '$lib/services/http-codes';
+
+
+export const DELETE: RequestHandler = async ({ url }) => {
+    const id = url.searchParams.get( 'id' );
+
+    if ( !id ) {
+        return json({ error: 'ID is required' }, { status: 400 });
+    }
+
+    const data = await connectRequest({
+        endpoint    : `v1/assistances/${ id }`,
+        method      : METHOD.DELETE,
+        isInternal  : false
+    });
+
+    return json( data );
+};
