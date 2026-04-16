3d3464e24b05a780a80bedab0cc9f16b2f752d36
kevinkeyssx <keyss1700@gmail.com>
2026-04-15 21:55:13 -0400

    Get all assistance server added

---
+++ src/routes/api/assistances/get-all/+server.ts
@@ -0,0 +1,38 @@
+import { json } from '@sveltejs/kit';
+
+import type { 
+    RequestHandler 
+}                       from './$types';
+import connectRequest   from '$lib/services/fetch.service';
+import { METHOD }       from '$lib/services/http-codes';
+
+
+export const GET: RequestHandler = async ({ url }) => {
+    const page          = url.searchParams.get( 'page' ) || '1';
+    const size          = url.searchParams.get( 'size' ) || '10';
+    const qrType        = url.searchParams.get( 'qr_type' ) || '';
+    const memberQuery   = url.searchParams.get( 'member_query' ) || '';
+    const date          = url.searchParams.get( 'date' ) || '';
+
+    let endpoint = `v1/assistances/?page=${ page }&size=${ size }`;
+
+    if ( qrType ) {
+        endpoint += `&qr_type=${ encodeURIComponent( qrType ) }`;
+    }
+
+    if ( memberQuery ) {
+        endpoint += `&member_query=${ encodeURIComponent( memberQuery ) }`;
+    }
+
+    if ( date ) {
+        endpoint += `&date=${ encodeURIComponent( date ) }`;
+    }
+
+    const data = await connectRequest({
+        endpoint,
+        method      : METHOD.GET,
+        isInternal  : false
+    });
+
+    return json( data );
+};
