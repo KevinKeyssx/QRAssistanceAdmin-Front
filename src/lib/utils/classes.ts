54adcc8eee3c1d4896a4a1e4e586a5bb7eff20ee
kevinkeyssx <keyss1700@gmail.com>
2026-04-20 17:16:29 -0400

    LDS_EXTRAS & LDS_ALL_CLASSES & getClassName & icons implemented

--- src/lib/utils/classes.ts
+++ src/lib/utils/classes.ts
@@ -1,12 +1,21 @@
-import type { LDSClass } from '$lib/types';
-import QuorumIcon        from '$lib/icons/QuorumIcon.svelte';
-import SOCIcon           from '$lib/icons/SOCIcon.svelte';
-import SchoolIcon        from '$lib/icons/SchoolIcon.svelte';
-import YoungWomenIcon    from '$lib/icons/YoungWomenIcon.svelte';
-import YoungMenIcon      from '$lib/icons/YoungMenIcon.svelte';
+import {
+    BookHeart,
+    Drama,
+    HandHeart,
+    Tickets
+} from 'lucide-svelte';
+
 // import PrimaryIcon       from '$lib/icons/PrimaryIcon.svelte';
-import JASIcon           from '$lib/icons/JASIcon.svelte';
-import FriendsIcon       from '$lib/icons/FriendsIcon.svelte';
+import type { LDSClass }    from '$lib/types';
+import QuorumIcon           from '$lib/icons/QuorumIcon.svelte';
+import SOCIcon              from '$lib/icons/SOCIcon.svelte';
+import SchoolIcon           from '$lib/icons/SchoolIcon.svelte';
+import YoungWomenIcon       from '$lib/icons/YoungWomenIcon.svelte';
+import YoungMenIcon         from '$lib/icons/YoungMenIcon.svelte';
+import JASIcon              from '$lib/icons/JASIcon.svelte';
+import FriendsIcon          from '$lib/icons/FriendsIcon.svelte';
+import MeetingIcon          from '$lib/icons/MeetingIcon.svelte';
+import FamilyIcon           from '$lib/icons/FamilyIcon.svelte';
 
 
 export const LDS_CLASSES: LDSClass[] = [
@@ -20,3 +29,23 @@ export const LDS_CLASSES: LDSClass[] = [
 	{ slug: 'friends',              label: 'Clase amigos',                      icon: FriendsIcon,    classCompatible: [ 'quorum-elders', 'relief-society', 'sunday-school-adults' ] },
 	// { slug: 'primary',              label: 'Primaria',                  icon: PrimaryIcon,    classCompatible: [] },
 ];
+
+
+export const LDS_EXTRAS: LDSClass[] = [
+    { slug: 'family-home',  label: 'Noche de Hogar',    icon: FamilyIcon,   classCompatible: [] },
+    { slug: 'show',         label: 'Show',              icon: Drama,        classCompatible: [] },
+    { slug: 'event',        label: 'Evento',            icon: Tickets,      classCompatible: [] },
+    { slug: 'meeting',      label: 'Reunión',           icon: MeetingIcon,  classCompatible: [] },
+    { slug: 'service',      label: 'Servicio',          icon: HandHeart,    classCompatible: [] },
+    { slug: 'other',        label: 'Otro',              icon: BookHeart,    classCompatible: [] },
+];
+
+
+export const LDS_ALL_CLASSES = [...LDS_CLASSES, ...LDS_EXTRAS];
+
+
+export const getClassName = ( classes: string | undefined | null ): string =>
+    !classes
+        ? ''
+        : LDS_ALL_CLASSES.find( ( c ) => c.slug === classes )?.label
+                ?? '';
